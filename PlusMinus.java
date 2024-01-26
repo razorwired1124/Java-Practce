6f548233349117c3a24d66a29eeb8df5d6e4b3cf
razorwired1124 <razorwired1124@gmail.com>
2024-01-26 05:44:36 -0600

    Plus Minus Problem - Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.

---
+++ PlusMinus.java
@@ -0,0 +1,56 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlusMinus {
+    public static void plusMinusZero(List<Integer> arr) {
+        // Write your code here
+        arr.add(1);
+        arr.add(-1);
+        arr.add(0);
+        arr.add(5);
+        arr.add(-4);
+        arr.add(3);
+        double p = 0;
+        double n = 0;
+        double z = 0;
+        double t = 0;
+        /*
+                for (int i = 0; i < arr.size(); i++) {
+            if (Integer.signum(arr.get(i)) == 1.0) {
+                p++;
+                t++;
+            } else if (Integer.signum(arr.get(i)) == -1.0) {
+                n++;
+                t++;
+            } else {
+                z++;
+                t++;
+            }
+        }
+         */
+
+        for (Integer integer : arr) {
+            if (Integer.signum(integer) == 1.0) {
+                p++;
+                t++;
+            } else if (Integer.signum(integer) == -1.0) {
+                n++;
+                t++;
+            } else {
+                z++;
+                t++;
+            }
+        }
+
+        DecimalFormat df = new DecimalFormat("0.000000");
+        System.out.println("POSITIVE %: " + df.format(p / t));
+        System.out.println("NEGATIVE %: " + df.format(n / t));
+        System.out.println("ZERO %: " + df.format(z / t));
+
+    }
+
+    public static void main(String[] args) {
+        PlusMinus.plusMinusZero(new ArrayList<>());
+    }
+}
